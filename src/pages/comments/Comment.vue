da98549a16a221bb974f59f56d7fa10de9a18c8e
Jerome <raoju1580@qq.com>
2018-08-05 15:04:06 +0800

    初始化页面骨架，添加tabBar，封装wx.request请求

---
+++ src/pages/comments/Comment.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      评论过的书
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Me",
+    create(){
+      console.log('个人中心页面');
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/util.js
@@ -0,0 +1,18 @@
+// 工具函数库
+
+import  config from './config'
+
+export function get(url) {
+  return new Promise((resolve,reject)=>{
+    wx.request({
+      url: config.host + url, //仅为示例，并非真实的接口地址
+      success: function(res) {
+        if(res.data.code === 0){
+          resolve(res.data.data)
+        }else{
+          reject(res.data)
+        }
+      }
+    })
+  })
+}
