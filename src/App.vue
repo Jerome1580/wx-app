bce168b9cc8557d0296b2502ae77c43de91cf02c
Jerome <raoju1580@qq.com>
2018-08-15 14:31:42 +0800

    格式化文件

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,35 @@
 <script>
-  import { get } from './util'
+import { get } from "./util";
 
 export default {
-    async created () {
-      // const res = await get("/weapp/demo");
-      // console.log(123,res);
-      console.log('小程序启动了')
+  async created() {
+    console.log("小程序启动了");
   }
-  }
+};
 </script>
 
 <style>
-.text-primary{
-	color:#EA5A49;
+.text-primary {
+  color: #ea5a49;
 }
-.text-footer{
-	margin-bottom: 5px;
-	text-align: center;
-	font-size: 12px;
+.text-footer {
+  margin-bottom: 5px;
+  text-align: center;
+  font-size: 12px;
 }
-.btn{
-	color: white;
-	background: #EA5A49;
-	margin-bottom: 10px;
-	padding-left: 15px;
-	padding-top: 7px;
-	border-radius: 2px;
-	font-size: 16px;
-	height: 40px;
-	width: 100%;
+.btn {
+  color: white;
+  background: #ea5a49;
+  margin-bottom: 10px;
+  padding-left: 15px;
+  padding-top: 7px;
+  border-radius: 2px;
+  font-size: 16px;
+  height: 40px;
+  width: 100%;
 }
 
-.btn.active{
-	background: #FA5A49;
+.btn.active {
+  background: #fa5a49;
 }
 </style>
